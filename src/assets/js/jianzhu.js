0c8e0c667789d1cf2a0fc252a96150ad3f0d13cb
bugmaker <289115128@qq.com>
2021-03-06 18:04:35 +0800

    计算所需合成设备数量

--- src/assets/js/jianzhu.js
+++ src/assets/js/jianzhu.js
@@ -1,94 +1,87 @@
 import zujian from "@/assets/js/zujian"
 
-const leixing = {
-    yelianshebei: "冶炼设备",
-    zhizaotai: "制造台",
-    keyanshebei: "科研设备",
-    caijiqi: "采矿机",
-    choushuizhan: "抽水设备",
-    chouyouzhan: "抽油设备",
-    jinglianshebei: "精炼设备",
-    huagongshebei: "化工设备",
-    fenliushebei: "分馏设备",
-    liziduizhuangji: "粒子对撞机",
-    shexianjieshouzhan: "射线接收站",
-    weiding: "未定"
-}
-
 const dianliganyingta = {
     jianzhu: true, name: "电力感应塔",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 2}, {wupin: zujian.cixianquan, shuliang: 1}],
         shijian: 1,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const wuxianshudianta = {
     jianzhu: true, name: "无线输电塔",
     info: [{
-        yuanliao: [{wupin: dianliganyingta, shuliang: 1}, {wupin: zujian.dianjiangfasheqi, shuliang: 1}],
-        shijian: 1,
-        leixing: leixing.zhizaotai
+        shuliang:1,
+        yuanliao: [{wupin: dianliganyingta, shuliang: 1}, {wupin: zujian.dianjiangfasheqi, shuliang: 3}],
+        shijian: 3,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const weixingpeidianzhan = {
     jianzhu: true, name: "微型配电站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: wuxianshudianta, shuliang: 1}, {
             wupin: zujian.chaojicichanghuan,
             shuliang: 10
         }, {wupin: zujian.kuangjiacailiao, shuliang: 2}],
         shijian: 5,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const fengliwolunji = {
     jianzhu: true, name: "风力涡轮机",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 6}, {wupin: zujian.chilun, shuliang: 1}, {
             wupin: zujian.cixianquan,
             shuliang: 3
         }],
         shijian: 4,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const huolifadianchang = {
     jianzhu: true, name: "火力发电厂",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 10}, {wupin: zujian.shicai, shuliang: 4}, {
             wupin: zujian.chilun,
             shuliang: 4
         }, {wupin: zujian.cixianquan, shuliang: 4}],
         shijian: 5,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const taiyangnengban = {
     jianzhu: true, name: "太阳能板",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tongkuai, shuliang: 6}, {
             wupin: zujian.gaochunguikuai,
             shuliang: 6
         }, {wupin: zujian.dianluban, shuliang: 4}],
         shijian: 5,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const xudianqi = {
     jianzhu: true, name: "蓄电器",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 6}, {
             wupin: zujian.chaojicichanghuan,
             shuliang: 6
         }, {wupin: zujian.jinggegui, shuliang: 4}],
         shijian: 5,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const shexianjieshouzhan = {
     jianzhu: true, name: "射线接收站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 20}, {
             wupin: zujian.gaochunguikuai,
             shuliang: 20
@@ -97,132 +90,145 @@ const shexianjieshouzhan = {
             shuliang: 5
         }, {wupin: zujian.chaojicichanghuan, shuliang: 20}],
         shijian: 8,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const weixingjubianfadianzhan = {
     jianzhu: true, name: "微型聚变发电站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.taihejin, shuliang: 12}, {
             wupin: zujian.chaojicichanghuan,
             shuliang: 10
         }, {wupin: zujian.tannamiguan, shuliang: 8}, {wupin: zujian.chuliqi, shuliang: 4}],
         shijian: 10,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const nengliangshuniu = {
     jianzhu: true, name: "能量枢纽",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.taihejin, shuliang: 40}, {
             wupin: zujian.gangcai,
             shuliang: 40
         }, {wupin: zujian.chuliqi, shuliang: 40}, {wupin: zujian.lizirongqi, shuliang: 8}],
         shijian: 15,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const renzaohengxing = {
     jianzhu: true, name: "人造恒星",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.taihejin, shuliang: 20}, {
             wupin: zujian.kuangjiacailiao,
             shuliang: 20
         }, {wupin: zujian.yanmieyueshuqiu, shuliang: 10}, {wupin: zujian.liangzixinpian, shuliang: 10}],
         shijian: 30,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const chuansongdai = {
     jianzhu: true, name: "传送带",
     info: [{
-        yuanliao: [{wupin: zujian.tiekuai, shuliang: 2 / 3}, {wupin: zujian.chilun, shuliang: 1 / 3}],
-        shijian: 1 / 3,
-        leixing: leixing.zhizaotai
+        shuliang:3,
+        yuanliao: [{wupin: zujian.tiekuai, shuliang: 2}, {wupin: zujian.chilun, shuliang: 1}],
+        shijian: 1,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const gaosuchuansongdai = {
     jianzhu: true, name: "传送带（高速）",
     info: [{
-        yuanliao: [{wupin: chuansongdai, shuliang: 1}, {wupin: zujian.dianciwolun, shuliang: 1 / 3}],
-        shijian: 1 / 3,
-        leixing: leixing.zhizaotai
+        shuliang:3,
+        yuanliao: [{wupin: chuansongdai, shuliang: 3}, {wupin: zujian.dianciwolun, shuliang: 1}],
+        shijian: 1,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const jisuchuansongdai = {
     jianzhu: true, name: "传送带（极速）",
     info: [{
-        yuanliao: [{wupin: gaosuchuansongdai, shuliang: 1}, {
+        shuliang:3,
+        yuanliao: [{wupin: gaosuchuansongdai, shuliang: 3}, {
             wupin: zujian.chaojicichanghuan,
-            shuliang: 1 / 3
-        }, {wupin: zujian.shimoxi, shuliang: 1 / 3}],
-        shijian: 1 / 3,
-        leixing: leixing.zhizaotai
+            shuliang: 1
+        }, {wupin: zujian.shimoxi, shuliang: 1 }],
+        shijian: 1 ,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const sixiangfenliuqi = {
     jianzhu: true, name: "四向分流器",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 3}, {wupin: zujian.chilun, shuliang: 2}, {
             wupin: zujian.dianluban,
             shuliang: 1
         }],
         shijian: 2,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const xiaoxingchuwucang = {
     jianzhu: true, name: "小型储物仓",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 4}, {wupin: zujian.shicai, shuliang: 4}],
         shijian: 2,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const daxingchuwucang = {
     jianzhu: true, name: "大型储物仓",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 8}, {wupin: zujian.shicai, shuliang: 8}],
         shijian: 4,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const chuyeguan = {
     jianzhu: true, name: "储液罐",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 8}, {wupin: zujian.shicai, shuliang: 4}, {
             wupin: zujian.boli,
             shuliang: 4
         }],
         shijian: 2,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const dianciguidaotansheqi = {
     jianzhu: true, name: "电磁轨道弹射器",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 20}, {wupin: zujian.chilun, shuliang: 20}, {
             wupin: zujian.chuliqi,
             shuliang: 5
         }, {wupin: zujian.chaojicichanghuan, shuliang: 10}],
         shijian: 6,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const xingxingneiwuliuyunshuzhan = {
     jianzhu: true, name: "行星内物流运输站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 40}, {wupin: zujian.taikuai, shuliang: 40}, {
             wupin: zujian.chuliqi,
             shuliang: 40
         }, {wupin: zujian.lizirongqi, shuliang: 20}],
         shijian: 20,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const weixingliziduizhuangji = {
     jianzhu: true, name: "微型粒子对撞机",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.taihejin, shuliang: 20}, {
             wupin: zujian.kuangjiacailiao,
             shuliang: 20
@@ -231,187 +237,204 @@ const weixingliziduizhuangji = {
             shuliang: 10
         }, {wupin: zujian.chuliqi, shuliang: 8}],
         shijian: 15,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const fenjianqi = {
     jianzhu: true, name: "分拣器",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 1}, {wupin: zujian.dianluban, shuliang: 1}],
         shijian: 1,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const gaosufenjianqi = {
     jianzhu: true, name: "分拣器（高速）",
     info: [{
-        yuanliao: [{wupin: fenjianqi, shuliang: 1}, {wupin: zujian.diandongji, shuliang: 0.5}],
-        shijian: 0.5,
-        leixing: leixing.zhizaotai
+        shuliang:2,
+        yuanliao: [{wupin: fenjianqi, shuliang: 2}, {wupin: zujian.diandongji, shuliang: 1}],
+        shijian: 1,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const jisufenjianqi = {
     jianzhu: true, name: "分拣器（极速）",
     info: [{
-        yuanliao: [{wupin: gaosufenjianqi, shuliang: 1}, {wupin: zujian.dianciwolun, shuliang: 0.5}],
-        shijian: 0.5,
-        leixing: leixing.zhizaotai
+        shuliang:2,
+        yuanliao: [{wupin: gaosufenjianqi, shuliang: 2}, {wupin: zujian.dianciwolun, shuliang: 1}],
+        shijian: 1,
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const caikuangji = {
     jianzhu: true, name: "采矿机",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 4}, {
             wupin: zujian.dianluban,
             shuliang: 2
         }, {wupin: zujian.cixianquan, shuliang: 2}, {wupin: zujian.chilun, shuliang: 2}],
         shijian: 3,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const choushuizhan = {
     jianzhu: true, name: "抽水站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 8}, {wupin: zujian.shicai, shuliang: 4}, {
             wupin: zujian.diandongji,
             shuliang: 4
         }, {wupin: zujian.dianluban, shuliang: 2}],
         shijian: 4,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const yuanyoucuiquzhan = {
     jianzhu: true, name: "原油萃取站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 12}, {
             wupin: zujian.shicai,
             shuliang: 12
         }, {wupin: zujian.dianluban, shuliang: 6}, {wupin: zujian.dianjiangfasheqi, shuliang: 4}],
         shijian: 8,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const yuanyoujinglianchang = {
     jianzhu: true, name: "原油精炼厂",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 10}, {
             wupin: zujian.shicai,
             shuliang: 10
         }, {wupin: zujian.dianluban, shuliang: 6}, {wupin: zujian.dianjiangfasheqi, shuliang: 6}],
         shijian: 6,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const chuizhifashejing = {
     jianzhu: true, name: "垂直发射井",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.taihejin, shuliang: 80}, {
             wupin: zujian.kuangjiacailiao,
             shuliang: 30
         }, {wupin: zujian.yinlitoujing, shuliang: 20}, {wupin: zujian.liangzixinpian, shuliang: 10}],
         shijian: 30,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const xingjiwuliuyunshuzhan = {
     jianzhu: true, name: "星际物流运输站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: xingxingneiwuliuyunshuzhan, shuliang: 1}, {
             wupin: zujian.taihejin,
             shuliang: 40
         }, {wupin: zujian.lizirongqi, shuliang: 20}],
         shijian: 30,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 
 const zhizaotaimk1 = {
     jianzhu: true, name: "制造台Mk.I",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 4}, {wupin: zujian.chilun, shuliang: 8}, {
             wupin: zujian.dianluban,
             shuliang: 4
         }],
         shijian: 2,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const zhizaotaimk2 = {
     jianzhu: true, name: "制造台Mk.II",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zhizaotaimk1, shuliang: 1}, {wupin: zujian.shimoxi, shuliang: 8}, {
             wupin: zujian.chuliqi,
             shuliang: 4
         }],
         shijian: 3,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const zhizaotaimk3 = {
     jianzhu: true, name: "制造台Mk.III",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zhizaotaimk2, shuliang: 1}, {
             wupin: zujian.lizikuandai,
             shuliang: 8
         }, {wupin: zujian.liangzixinpian, shuliang: 2}],
         shijian: 4,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 
 const dianhuronglu = {
     jianzhu: true, name: "电弧熔炉",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 4}, {wupin: zujian.shicai, shuliang: 2}, {
             wupin: zujian.dianluban,
             shuliang: 4
         }, {wupin: zujian.cixianquan, shuliang: 2}],
         shijian: 3,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const fenliuta = {
     jianzhu: true, name: "分馏塔",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 8}, {wupin: zujian.shicai, shuliang: 4}, {
             wupin: zujian.boli,
             shuliang: 4
         }, {wupin: zujian.chuliqi, shuliang: 1}],
         shijian: 3,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const huagongchang = {
     jianzhu: true, name: "化工厂",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.gangcai, shuliang: 8}, {wupin: zujian.shicai, shuliang: 8}, {
             wupin: zujian.boli,
             shuliang: 8
         }, {wupin: zujian.dianluban, shuliang: 2}],
         shijian: 5,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const juzhenyanjiuzhan = {
     jianzhu: true, name: "矩阵研究站",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: zujian.tiekuai, shuliang: 8}, {wupin: zujian.boli, shuliang: 4}, {
             wupin: zujian.dianluban,
             shuliang: 4
         }, {wupin: zujian.cixianquan, shuliang: 4}],
         shijian: 3,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 const guidaocaijiqi = {
     jianzhu: true, name: "轨道采集器",
     info: [{
+        shuliang:1,
         yuanliao: [{wupin: xingjiwuliuyunshuzhan, shuliang: 1}, {
             wupin: zujian.chaojicichanghuan,
             shuliang: 50
         }, {wupin: zujian.jialituijinqi, shuliang: 20}, {wupin: xudianqi, shuliang: 20}],
         shijian: 30,
-        leixing: leixing.zhizaotai
+        leixing: zujian.leixing.zhizaotai
     }]
 }
 export default {
--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,24 +1,77 @@
 <template>
     <div>
         <div>
-            <el-tag v-if="tree.info[dangqian].caiji==true" type="danger" size="mini">{{tree.name}}<span
-                    v-if="switchvalue.shuliang">*{{zongshuliang}}</span>
+            <!--如果是采集物品，显示红色tag-->
+            <el-tag v-if="tree.info[dangqian].caiji==true" type="danger" size="mini">
+                {{tree.name}}
+                <span v-if="switchvalue.shuliang">*{{switchvalue.quzheng?Math.ceil(zongshuliang):zongshuliang}}</span>
             </el-tag>
-            <el-tag v-else-if="tree.jianzhu==true" type="warning" size="mini">{{tree.name}}<span
-                    v-if="switchvalue.shuliang">*{{zongshuliang}}</span>
+            <!--如果是建筑，显示橙色tag-->
+            <el-tag v-else-if="tree.jianzhu==true" type="warning" size="mini">
+                {{tree.name}}
+                <span v-if="switchvalue.shuliang">*{{switchvalue.quzheng?Math.ceil(zongshuliang):zongshuliang}}</span>
             </el-tag>
-            <el-tag v-else size="mini">{{tree.name}}<span v-if="switchvalue.shuliang">*{{zongshuliang}}</span></el-tag>
-            <el-tag v-if="switchvalue.duoyu && tree.info[dangqian].duoyu!=undefined " type="info" size="mini"><span
-                    v-for="item in tree.info[dangqian].duoyu"
-                    :key="item.wupin">{{item.wupin}}<span
-                    v-if="switchvalue.shuliang">*{{item.shuliang*zongshuliang}}</span></span>
+            <!--其他的显示普通tag-->
+            <el-tag v-else size="mini">
+                {{tree.name}}
+                <span v-if="switchvalue.shuliang">*{{switchvalue.quzheng?Math.ceil(zongshuliang):zongshuliang}}</span>
+            </el-tag>
+            <!--显示多余产物-->
+            <el-tag v-if="switchvalue.duoyu && tree.info[dangqian].duoyu!=undefined" type="info" size="mini">
+                <span
+                        v-for="item in tree.info[dangqian].duoyu"
+                        :key="item.wupin">
+                    {{item.wupin}}
+                    <!--多余产物数量=单位多余产物数量/单位物品数量*总数量-->
+                    <span v-if="switchvalue.shuliang">*
+                        <span v-if="switchvalue.quzheng">{{Math.ceil(item.shuliang*zongshuliang/tree.info[dangqian].shuliang)}}</span>
+                        <span v-else>{{item.shuliang*zongshuliang/tree.info[dangqian].shuliang}}</span>
+                    </span>
+                </span>
             </el-tag>
         </div>
+        <!--是否显示合成设备-->
         <div v-if="switchvalue.shebei">
-            <el-tag type="success" size="mini" effect="plain">{{tree.info[dangqian].leixing}}
-                <span v-if="!tree.info[dangqian].caiji">*{{zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang)}}</span>
+            <el-tag type="success" size="mini" effect="plain">
+                {{tree.info[dangqian].leixing}}
+                <!--1分钟产出所需合成设备数量=总数量/一台机器1分钟产出数量=总数量/（60/单位产物时间*单位产物数量）-->
+                <span v-if="!tree.info[dangqian].caiji">
+                    <span v-if="tree.info[dangqian].leixing=='制造台'">
+                        <span v-if="switchvalue.zhizaotai==0.75">Mk.I</span>
+                        <span v-if="switchvalue.zhizaotai==1">Mk.II</span>
+                        <span v-if="switchvalue.zhizaotai==1.5">Mk.III</span>
+                        *
+                        <span v-if="switchvalue.quzheng">{{Math.ceil(zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang*switchvalue.zhizaotai))}}</span>
+                        <span v-else>{{zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang*switchvalue.zhizaotai)}}</span>
+                    </span>
+                    <span v-else>
+                        *
+                        <span v-if="switchvalue.quzheng">{{Math.ceil(zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang))}}</span>
+                        <span v-else>{{zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang)}}</span>
+                    </span>
+                </span>
+                <span v-else-if="tree.info[dangqian].leixing=='采矿机'">
+                    (矿脉*
+                    <span v-if="switchvalue.quzheng">{{Math.ceil(zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang))}}</span>
+                    <span v-else>{{zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang)}}</span>
+                    )
+                </span>
+                <span v-else-if="tree.info[dangqian].leixing=='抽水设备'">
+                    *
+                    <span v-if="switchvalue.quzheng">{{Math.ceil(zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang))}}</span>
+                    <span v-else>{{zongshuliang/(60/tree.info[dangqian].shijian*tree.info[dangqian].shuliang)}}</span>
+                </span>
+                <span v-else-if="tree.info[dangqian].leixing=='抽油设备'">
+                    (原油涌泉产量>
+                    <span v-if="switchvalue.quzheng">{{Math.round(zongshuliang/60/1.5*100)/100}}</span>
+                    <span v-else>{{zongshuliang/60/1.5}}</span>
+                    /s)</span>
+                <span v-else-if="tree.info[dangqian].leixing=='轨道采集器'">(没开采过，知道的给我说说)</span>
+                <span v-else-if="tree.info[dangqian].leixing=='射线接收站'">(还没到这，知道的给我说说)</span>
+                <span v-else-if="tree.info[dangqian].leixing=='手采'">(手动采集没效率，要不还是算了？)</span>
             </el-tag>
         </div>
+        <!--提供切换合成方式按钮-->
         <div v-if="tree.info.length>1" style="white-space: nowrap;">
             <el-link type="primary" @click="qiehuanhechengfangshi(-1)"><i class="el-icon-caret-left"></i></el-link>
             <el-tag type="info" size="mini" effect="plain">{{tree.info[dangqian].zhushi}}</el-tag>
@@ -26,6 +79,7 @@
         </div>
         <div v-if="tree.info[dangqian].yuanliao.length>1">丨</div>
         <div class="yuanliao">
+            <!--遍历原料-->
             <div class="a" v-for="(item,index) in tree.info[dangqian].yuanliao" :key="index">
                 <div v-if="index==0 && tree.info[dangqian].yuanliao.length>1" class="border-box">
                     <div style="width: 100%;"></div>
@@ -42,6 +96,8 @@
                     <div style="border-top:1px black solid;width: 100%;"></div>
                 </div>
                 <div>丨</div>
+                <!--shuliang:合成一个物品所需某项原料数量=单位原料数量/单位物品数量-->
+                <!--zongshuliang:合成当前所有物品所需某项原料总数量=单位原料数量/单位物品数量*需合成的总数量-->
                 <Tree :tree="item.wupin"
                       :shuliang="item.shuliang/tree.info[dangqian].shuliang"
                       :zongshuliang="item.shuliang * zongshuliang/tree.info[dangqian].shuliang"
